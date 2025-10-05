f31fef4221f540a9ac17555f0b5c7e206d9b3d8a
Arjun V Reddy <reddyavr2003@gmail.com>
2025-10-05 20:01:21 +0530

    Prepare backend for Render

--- app.py
+++ app.py
@@ -101,5 +101,4 @@ def publications_stats():
 
 # Run app
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host="0.0.0.0", port=port)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
